fd6c2e3c767a2f412bedcd9f94f4fe5251c3b4da
kimusann0604 <fantianmucun@gmail.com>
2024-09-13 12:51:50 +0900

    first commit

---
+++ ellipse.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+
+img = np.zeros((300, 300, 3), dtype=np.uint8)
+
+cv2.ellipse(
+    img,
+    (150, 150),
+    (50, 70),
+    angle=90,
+    startAngle=250,
+    endAngle=100,
+    color=(255, 0, 0),
+    thickness=2,
+)
+cv2.imshow("Ellipse", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()      
